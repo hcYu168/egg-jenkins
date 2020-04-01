2d3e361760d7ed6cfc26035159807e69da964c1d
yhc <731636814@qq.com>
2020-04-01 17:52:40 +0800

    test webhook2

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    console.log('test webhook');
+    console.log('test webhook2');
     ctx.body = {
       code: 200,
       data: {
