f7d35d04f6c6a97d8490c94819a5c0b3eaa10a32
yhc <731636814@qq.com>
2020-04-02 17:27:12 +0800

    add test-index

--- app/controller/test.js
+++ app/controller/test.js
@@ -1,6 +1,8 @@
 'use strict';
 const Controller = require('egg').Controller;
 class TestController extends Controller {
-
+  async index() {
+    this.ctx.body = 200;
+  }
 }
 module.exports = TestController;
--- app/service/test.js
+++ app/service/test.js
@@ -1,6 +1,8 @@
 'use strict';
 const Service = require('egg').Controller;
 class TestService extends Service {
-
+  async index() {
+    return 200;
+  }
 }
 module.exports = TestService;
